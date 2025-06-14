8c121b8fe4fd347f3df7e471aab5e8e793519031
lantoniomxnusd <luisoteacher@gmail.com>
2025-06-14 13:54:45 -0600

    Created the filter for the mode so that it selects either by level and unit or by category then creates an array with the results.

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,21 @@
 import { createCard, shuffle } from "./cards.mjs";
 
+let allWords = [];
+
+async function loadData(){
+  const res = await fetch('levels/ourworld-master.json');
+  allWords = await res.json();
+}
+
+function filterWords(mode,selected){
+  if (mode === 'unit'){
+    const [level,unit] = selected.match(/\d+/g);
+    return allWords.filter(word => word.level === selected && word.unit === unit);
+  }else {
+    return allWords.filter(word => word.category === selected);
+  }
+}
+
 const cardElement = createCard({type:'text', content: 'Dog', word: 'dog'}, 0);
 document.getElementById('gameBoard').appendChild(cardElement);
 
