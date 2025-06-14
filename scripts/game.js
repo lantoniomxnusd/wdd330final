80536a0685dc103a2a09cffb12f286048a2cc2c7
lantoniomxnusd <luisoteacher@gmail.com>
2025-06-14 13:36:37 -0600

    created a shuffle function for the cards.

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,11 @@
+import { createCard, shuffle } from "./cards.mjs";
+
+const cardElement = createCard({type:'text', content: 'Dog', word: 'dog'}, 0);
+document.getElementById('gameBoard').appendChild(cardElement);
+
 document.querySelectorAll('.card').forEach(card => {
   card.addEventListener('click', () => {
     card.classList.toggle('flipped');
   });
 });
+
