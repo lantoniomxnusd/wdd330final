bcf294787afc17b0f95fddd02bb1d5d0f79e2cf2
lantoniomxnusd <luisoteacher@gmail.com>
2025-06-14 16:57:54 -0600

    User interface created with instructions that will be collapsable in the future (working on functionality before the aesthetics). Added a mode that the user can choose between "Our World" which is the current learning material for my young students and "Category" so the user can practice based on category.

--- scripts/game.js
+++ scripts/game.js
@@ -10,18 +10,41 @@ async function loadData(){
 function filterWords(mode,selected){
   if (mode === 'unit'){
     const [level,unit] = selected.match(/\d+/g);
-    return allWords.filter(word => word.level === selected && word.unit === unit);
+    return allWords.filter(word => word.level === level && word.unit === unit);
   }else {
     return allWords.filter(word => word.category === selected);
   }
 }
 
-const cardElement = createCard({type:'text', content: 'Dog', word: 'dog'}, 0);
-document.getElementById('gameBoard').appendChild(cardElement);
+async function setupGame(){
+  const mode = document.getElementById('mode').value;
+  const selected = document.getElementById('optionSelect').value;
 
-document.querySelectorAll('.card').forEach(card => {
-  card.addEventListener('click', () => {
-    card.classList.toggle('flipped');
-  });
-});
+  const words = filterWords(mode, selected);
 
+  const imageCards = words.map(item => ({
+    type: 'image', 
+    content: item.image,
+    word: item.word
+  }))
+
+  const textCards = words.map(item => ({
+    type: 'text',
+    content: item.word,
+    word: item.word
+  }));
+
+  const cards = shuffle ([...imageCards, ...textCards]);
+
+  const board = document.getElementById('gameBoard');
+  board.innerHTML = '';
+
+  cards.forEach((cardData, index) => {
+    const cardElement = createCard(cardData, index);
+    board.appendChild(cardElement);
+  })
+}
+
+document.getElementById('startGame').addEventListener('click', setupGame);
+
+loadData();
